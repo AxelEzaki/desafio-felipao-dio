6b358e9465493b1ae1dc7911b0a24aab7ecb62fd
axelezaki <axelkeza@hotmail.com>
2025-07-16 17:25:13 -0300

    Retirado alguns comentários inúteis

--- desafio-felipao/src/index.js
+++ desafio-felipao/src/index.js
@@ -68,37 +68,3 @@ function concluir(){
 }
 
 perguntarNome("Qual seu nome, herói?\n");
-
-// let qtdeXP = prompt("Quanto você tem de experiência, herói?");
-// while(nome == "" || nome == null){
-//     qtdeXP = prompt("Por favor, me informe quanto você tem de experiência, herói?");
-// }
-
-// let classificacao = "";
-// let xpConvertido = Number(qtdeXP);
-
-// if (typeof xpConvertido === 'number' && !isNaN(xpConvertido)) {
-//     if(xpConvertido <= 1000){
-//         classificacao = "Ferro";
-//     } else if(xpConvertido >= 1001 && xpConvertido <= 2000){
-//         classificacao = "Bronze";
-//     } else if(xpConvertido >= 2001 && xpConvertido <= 5000){
-//         classificacao = "Prata";
-//     } else if(xpConvertido >= 5001 && xpConvertido <= 7000){
-//         classificacao = "Ouro";
-//     } else if(xpConvertido >= 7001 && xpConvertido <= 8000){
-//         classificacao = "Platina";
-//     } else if(xpConvertido >= 8001 && xpConvertido <= 9000){
-//         classificacao = "Ascendente";
-//     } else if(xpConvertido >= 9001 && xpConvertido <= 10000){
-//         classificacao = "Imortal";
-//     }else if(xpConvertido >= 10001){
-//         classificacao = "Radiante";
-//     }
-// } else {
-//     console.log(`"${qtdeXP}" não é um valor numérico válido.`);
-// }
-
-// if(classificacao != ""){
-//     console.log(`O Herói de nome ${nome} está no nível de ${classificao}.`);
-// }
